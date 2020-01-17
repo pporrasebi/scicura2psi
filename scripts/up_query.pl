c49d4759b24e97ab73e2496a361ab9c7387d7297
Pablo Porras Millán <pporras@ebi.ac.uk>
2020-01-17 17:28:08 +0100

    updates on xml format and new relevant info found

--- scripts/up_query.pl
+++ scripts/up_query.pl
@@ -10,11 +10,11 @@ while(<FILE>) {
          chomp;
          my $query_term = $_;
          # query UniProt for the given identifier and retrieve tab-delimited format
-         system ("wget -O output -q  \"http://www.uniprot.org/uniprot/?query=id:$query_term&format=tab&columns=id,organism-id\"");
-         open(WGET, "output") || die "cannot open output";
-         while(<WGET>) {
+         system ("curl \"http://www.uniprot.org/uniprot/?query=id:$query_term&format=tab&columns=id,organism-id\" -o output");
+         open(CURL, "output") || die "cannot open output";
+         while(<CURL>) {
                  print $_ if (/[A-Z]/ && !/Entry/); # do not print header
          }
-         close(WGET);
+         close(CURL);
 }
 close(FILE);
